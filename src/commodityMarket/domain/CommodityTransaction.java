d67e2468eb19e1b0f13f0b77387cf301b8a58cb8
Javier Vela <javier.vela00@gmail.com>
2025-01-19 17:02:55 +0100

    Format source code

--- src/commodityMarket/domain/CommodityTransaction.java
+++ src/commodityMarket/domain/CommodityTransaction.java
@@ -2,51 +2,47 @@ package commodityMarket.domain;
 
 import java.io.Serializable;
 
-public class CommodityTransaction implements Serializable{
-	
-	//STATIC FIELDS
+public class CommodityTransaction implements Serializable {
 
-	//FIELDS
+	// STATIC FIELDS
+
+	// FIELDS
 	int supplier;
 	int commodity;
 	int quantity;
 	int consumer;
-	
-	
-	//CONSTRUCTORS
-	public CommodityTransaction(int supplier, int commodity, int quantity, int consumer){
+
+	// CONSTRUCTORS
+	public CommodityTransaction(int supplier, int commodity, int quantity, int consumer) {
 		this.supplier = supplier;
 		this.commodity = commodity;
 		this.quantity = quantity;
 		this.consumer = consumer;
 	}
-	
-	//METHODS
 
-	//GETTERS AND SETTERS
-	public int getSupplier(){
+	// METHODS
+
+	// GETTERS AND SETTERS
+	public int getSupplier() {
 		return this.supplier;
 	}
-	
-	public int getCommodity(){
+
+	public int getCommodity() {
 		return this.commodity;
 	}
-	
-	public int getQuantity(){
+
+	public int getQuantity() {
 		return this.quantity;
 	}
-	
-	public int getConsumer(){
+
+	public int getConsumer() {
 		return this.consumer;
 	}
-	
 
-	
-	
-	public String toString(){
-		
-		String s = "agent " + supplier + " gives " + quantity + " units of " + commodity + " to agent " + consumer; 
+	public String toString() {
+
+		String s = "agent " + supplier + " gives " + quantity + " units of " + commodity + " to agent " + consumer;
 		return s;
-		
+
 	}
 }
